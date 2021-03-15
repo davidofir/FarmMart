4f86e08e559e53c0b761875163ce6ff78e506c14
ofir <davidof@sheridancollege.ca>
2021-03-15 00:21:51 -0400

    added navigation

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import * as React from 'react';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { Button, StyleSheet, Text, View,TouchableOpacity } from 'react-native';
 import Colors from './constants/colors';
 import HomePage from './components/HomePage'
+import 'react-native-gesture-handler';
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View  style={styles.container}>
-      <HomePage/>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomePage}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
