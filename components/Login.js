dad8b214a3d7962e8a80c60d4b3c13f6b36ed5a5
ofir <davidof@sheridancollege.ca>
2021-03-15 12:41:07 -0400

    added the login screen

--- components/Login.js
+++ components/Login.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import {Text,View,TextInput} from 'react-native';
+import {Text,View,TextInput,StyleSheet} from 'react-native';
 const Login = props =>{return(
-    <View>
-        <Text>Login Page</Text>
+    <View style={styles.container}>
+        <View style={styles.inputs}>
+            <View styles={styles.input}>
+                <TextInput placeholder="UserName" autoCompleteType="username"/>
+            </View>
+            <View style={styles.input}>
+                <TextInput placeholder="Password" secureTextEntry={true} autoCompleteType="password"/>
+            </View>
+        </View>
     </View>
 )
 };
-
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent:"center",
+    },
+    inputs:{
+        marginBottom:200,
+        
+    },
+    input:{
+        padding:5
+    }
+  });
 export default Login;
