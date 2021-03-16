05284c68287113ce7bfed498fa1ff055d02e2adc
ofir <davidof@sheridancollege.ca>
2021-03-16 12:33:50 -0400

    fixed the onpress of the buttons

--- components/ButtonComponent.js
+++ components/ButtonComponent.js
@@ -3,8 +3,8 @@ import {View,TouchableOpacity,Text,StyleSheet} from 'react-native';
 import Colors from '../constants/colors';
 const buttonComponent = props =>{
     return(
-    <TouchableOpacity style={[styles.button,{backgroundColor:props.background,borderColor:props.borderColorStyle}]}>
-        <Text onPress={props.clickEvent} style={{color:props.textColor}}>{props.buttonTitle}</Text>
+    <TouchableOpacity onPress={props.clickEvent} style={[styles.button,{backgroundColor:props.background,borderColor:props.borderColorStyle}]}>
+        <Text  style={{color:props.textColor}}>{props.buttonTitle}</Text>
     </TouchableOpacity>
     );
 }
