84f2546c0b031cf0a26a8b8eb2a01811ff9bf5e2
ofir <davidof@sheridancollege.ca>
2021-03-30 13:48:48 -0400

    added firebase integration

--- App.js
+++ App.js
@@ -2,6 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC5_3euKIb8ScfRbQOM7AUqkBihbl2syf4",
+  authDomain: "farmmart-f2a33.firebaseapp.com",
+  projectId: "farmmart-f2a33",
+  storageBucket: "farmmart-f2a33.appspot.com",
+  messagingSenderId: "46976507025",
+  appId: "1:46976507025:web:329e6137319c16025e7330",
+  measurementId: "G-WFEMP1F5BS"
+};
+if(firebase.apps.length === 0){
+  firebase.initializeApp(firebaseConfig);
+}
 import { Button, StyleSheet, Text, View,TouchableOpacity } from 'react-native';
 import Login from './pages/Login'
 import Colors from './constants/colors';
